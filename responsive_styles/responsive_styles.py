268e49da5b1fecc68851f37f04a7550f18a6fcda
Jeremy Weinstein <jeremydw@gmail.com>
2020-02-12 15:06:26 -0800

    Get example working

--- responsive_styles/responsive_styles.py
+++ responsive_styles/responsive_styles.py
@@ -4,34 +4,30 @@ import jinja2.ext
 import os
 
 
+_attribute_cache = {}
+
+
 def responsive_style_id(queries_to_styles):
     pass
 
 
 @jinja2.contextfunction
-def render_responsive_styles(ctx, queries_to_styles):
-    pod = ctx.get('doc').pod
-    template_list = []
-    for macro in macros:
-        template_list.append(
-            '{%% import "/views/macros/%(macro)s.html" as %(macro)s_lib with context %%}' % {'macro': macro})
-    for macro in macros:
-        template_list.append(
-            '{%% macro %(macro)s(options) %%}{{%(macro)s_lib.%(macro)s(options, **kwargs)}}{%% endmacro %%}' % {'macro': macro})
-    template = pod.get_jinja_env().from_string(template_str)
-    return template.make_module(ctx, locals={'partial': partial})
+def get_attribute_cache(ctx):
+    return _attribute_cache
 
 
 @jinja2.contextfunction
-def responsive_style_attribute(ctx, queries_to_styles):
-    if not queries_to_styles:
+def responsive_style_attribute(ctx, styles):
+    if not styles:
         return ''
     doc = ctx.get('doc')
-    return '-rs' + hashlib.sha1(str(queries_to_styles)).hexdigest()[:4]
+    attribute = '-rs' + hashlib.sha1(str(styles)).hexdigest()[:4]
+    _attribute_cache[attribute] = styles
+    return attribute
 
 
 class ResponsiveStylesExtension(jinja2.ext.Extension):
     def __init__(self, env):
         super(ResponsiveStylesExtension, self).__init__(env)
         env.globals['responsive_style_attribute'] = responsive_style_attribute
-        env.globals['render_responsive_styles'] = render_responsive_styles
+        env.globals['get_attribute_cache'] = get_attribute_cache
